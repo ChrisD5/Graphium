4e2c76c618d9a3490ff12e23609afb817e762721
Kavin Sundar <sundark@cardiff.ac.uk>
2022-12-02 15:58:01 +0000

    Remove unused imports.

--- src/main/java/ai/graphium/checkin/controllers/HomeController.java
+++ src/main/java/ai/graphium/checkin/controllers/HomeController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @Controller
 public class HomeController {
 
@@ -27,8 +24,8 @@ public class HomeController {
         }
     }
 
-    @RequestMapping(value="/logout", method = RequestMethod.GET)
-    public String logoutPage () {
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage() {
         SecurityContextHolder.getContext().setAuthentication(null);
         return "redirect:/login";
     }
