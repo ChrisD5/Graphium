ef293380a5b3b68574d9d7491298157f5f6a6678
Kavin Sundar <sundark@cardiff.ac.uk>
2022-12-14 15:46:27 +0000

    Allow employee to see meetings and their notes.

--- src/main/java/ai/graphium/checkin/repos/MeetingRepository.java
+++ src/main/java/ai/graphium/checkin/repos/MeetingRepository.java
@@ -7,8 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+    List<Meeting> findByRequester_EmailOrderByTimeDesc(String email);
+
+    List<Meeting> findByRequester_EmailAndTimeLessThanOrderByTimeDesc(String email, long time);
+
     List<Meeting> findByRequester_EmailAndTimeGreaterThan(String email, long time);
 
     List<Meeting> findByRequestee(User requestee);
+
     List<Meeting> findByRequesteeOrderByTimeDesc(User requestee);
 }
